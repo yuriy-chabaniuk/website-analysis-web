5d2a1d522cc0be18b352c12f645e09efb0a6dab2
Yuriy Chabaniuk <y.chabaniuk@grt-team.com>
2017-11-07 19:42:43 -0500

    installed slim/slim

---
+++ app/Actions/ReportAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by: Yuriy Chabaniuk
+ */
+
+
+namespace App\Actions;
+
+
+class ReportAction {
+
+    public function __invoke() {
+        // TODO: Implement __invoke() method.
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,59 +3,16 @@
  * Created by: Yuriy Chabaniuk
  */
 
-require_once 'vendor/autoload.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpKernel;
+require 'vendor/autoload.php';
 
-define('ROOT_FOLDER', __DIR__ . '/../');
-define('PUBLIC_FOLDER', __DIR__);
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
 
-$request = Request::createFromGlobals();
-$response = new Response();
-
-$routes = require_once __DIR__ . '/../routes/web.php';
-
-$context = new RequestContext();
-$context->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
-
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-try {
-    // work around for assets.
-    $pathInfo = $_SERVER['REQUEST_URI'];
-    if (strpos($pathInfo, '/assets/') === 0) {
-        $file = PUBLIC_FOLDER . "{$pathInfo}";
-
-        if (is_readable($file)) {
-            echo file_get_contents($file);
-
-            return;
-        }
-
-        return new Response('No file.');
-    }
-
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $controllerResolver->getController($request);
-    if (is_object($controller[0])) {
-        $controller[0]->setRequest($request)
-            ->setRequestContext($context);
-    }
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (Routing\Exception\ResourceNotFoundException $e) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $e) {
-    $response = new Response($e->getMessage(), 500);
-}
-
-$response->send();
+    return $response;
+});
+$app->run();
